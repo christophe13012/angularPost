12b8188e00285fb95e43e94eb8afa7c739fb63cf
chris13012 <christophecaillet@icloud.com>
2019-09-04 11:13:59 +0200

    firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'blog';
+  title = "blog";
+  posts: Posts[];
+
+  constructor() {
+    this.posts = [
+      {
+        title: "Titre du 1er post",
+        content:
+          "Dolor esse commodo esse adipisicing reprehenderit ea occaecat sit.",
+        loveIts: 2,
+        created_at: new Date()
+      },
+      {
+        title: "2ieme post",
+        content: "Id ea incididunt deserunt eu Lorem.",
+        loveIts: -3,
+        created_at: new Date()
+      },
+      {
+        title: "3ieme post",
+        content: "Sit deserunt enim veniam exercitation id aliquip non culpa.",
+        loveIts: 0,
+        created_at: new Date()
+      }
+    ];
+  }
+}
+
+export class Posts {
+  title: string;
+  content: string;
+  loveIts: number;
+  created_at: Date;
 }
